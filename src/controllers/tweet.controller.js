01a686b48f7d84a555cf384414d5f92c0000f792
pradeep <pradeep>
2024-01-09 18:11:42 +0530

    fixed get user tweets by mongodb aggregation pipeline

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -5,14 +5,14 @@ import { apiResponse } from "../utils/apiResponce.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createTweet = asyncHandler(async (req, res) => {
-  const  {content}  = req.body;
+  const { content } = req.body;
 
-  if (!content || typeof content !== 'string') {
-    return res.status(400).json(new apiError(400,  "Tweet content is required"));
+  if (!content || typeof content !== "string") {
+    return res.status(400).json(new apiError(400, "Tweet content is required"));
   }
 
   if (!req.user) {
-     return res.status(400).json(new apiError(400, "User is required"));
+    return res.status(400).json(new apiError(400, "User is required"));
   }
 
   const tweet = await Tweet.create({
@@ -24,61 +24,70 @@ const createTweet = asyncHandler(async (req, res) => {
     throw new apiError("Tweet not created");
   }
 
-  return res
-    .status(201)
-    .json(new apiResponse(true, tweet, "Tweet created"));
+  return res.status(201).json(new apiResponse(true, tweet, "Tweet created"));
 });
-
 
 const getUserTweets = asyncHandler(async (req, res) => {
   // TODO: get user tweets
-  const user = req.user;
-  if (!user) {
-    throw new apiError("user can't find, pls login");
+  const { userId } = req.params;
+  if (!userId) {
+    throw new apiError("userID can't find OR invalid user ID");
   }
 
-  const tweets = await Tweet.findOne({ owner: user?._id })
-
-  if (!tweets) {
-    throw new apiError("tweets not found");
-  
-  }
-
+  const userTweets = await Tweet.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "user",
+      },
+    },
+    {
+      $project: {
+        content: 1,
+        createdAt: 1,
+        owner: 1,
+        username: { $arrayElemAt: ["$user.username", 0] },
+        profilePicture: { $arrayElemAt: ["$user.avatar", 0] },
+      },
+    },
+  ]);
   return res
   .status(200)
-  .json(new apiResponse(true, tweets, "tweets found"));
-
+  .json(new apiResponse(true, userTweets,"user tweets"));
 });
 
 const updateTweet = asyncHandler(async (req, res) => {
   //TODO: update tweet
-  const {content} = req.body;
+  const { content } = req.body;
   if (!content) {
     throw new apiError("pls write something");
   }
-  if(!req.user){
+  if (!req.user) {
     throw new apiError("user can't find, pls login");
   }
-  const tweet = await Tweet.findOneAndUpdate({owner:req.user?._id},
-    {content},
-    {new:true}
-    )
+  const tweet = await Tweet.findOneAndUpdate(
+    { owner: req.user?._id },
+    { content },
+    { new: true }
+  );
 
-    if(!tweet){
-        throw new apiError("tweet not found");
-    }
+  if (!tweet) {
+    throw new apiError("tweet not found");
+  }
 
-
-    return res
-    .status(200)
-    .json(new apiResponse(true, tweet, "tweet updated"));
-
-
+  return res.status(200).json(new apiResponse(true, tweet, "tweet updated"));
 });
 
 const deleteTweet = asyncHandler(async (req, res) => {
   //TODO: delete tweet
-  const {tweetId} = req.params;
+  const { tweetId } = req.params;
   if (!tweetId) {
     throw new apiError("cant find tweet id");
   }
@@ -91,21 +100,16 @@ const deleteTweet = asyncHandler(async (req, res) => {
     throw new apiError("user can't find, pls login");
   }
 
-  const tweet = await Tweet.findOneAndDelete(
-    { 
-        _id: tweetId ,
-        owner: authenticatedId?._id
-    }
-    )
+  const tweet = await Tweet.findOneAndDelete({
+    _id: tweetId,
+    owner: authenticatedId?._id,
+  });
 
-    if(!tweet){
-        throw new apiError("tweet not found");
-    }
+  if (!tweet) {
+    throw new apiError("tweet not found");
+  }
 
-    return res
-    .status(200)
-    .json(new  apiResponse(200, "tweet deleted"));
-
+  return res.status(200).json(new apiResponse(200, "tweet deleted"));
 });
 
 export { createTweet, getUserTweets, updateTweet, deleteTweet };
