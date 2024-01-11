de16e1bb8422b7a1e5183c298681ba93b1560517
pradeep <pradeep>
2024-01-11 15:46:47 +0530

    every controller is working fine

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,84 +1,102 @@
 import { apiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { Subscription as subscriptionModel} from "../models/subscription.model.js";
+import { Subscription as subscriptionModel } from "../models/subscription.model.js";
 import { apiResponse } from "../utils/apiResponce.js";
 import mongoose from "mongoose";
 
-
 const toggleSubscription = asyncHandler(async (req, res) => {
-    // TODO: toggle subscription
-    const {channelId} = req.params
-    const user = req.user;
-    if(!user){
-        throw new apiError(401, "user not found")
-    }
-    if(!mongoose.isValidObjectId(channelId) || !channelId){
-        throw new apiError(401, "channel not found")
-    }
+  // TODO: toggle subscription
+  const { channelId } = req.params;
+  const user = req.user;
+  if (!user) {
+    throw new apiError(401, "user not found");
+  }
+  if (!mongoose.isValidObjectId(channelId) || !channelId) {
+    throw new apiError(401, "channel not found");
+  }
 
-    const existingSubscription = await subscriptionModel.findOneAndDelete({
-        subscriber: user?._id,
-        channel: channelId
-    })
-    
-    let message = ""
-    let newsubscription = null;
+  const existingSubscription = await subscriptionModel.findOneAndDelete({
+    subscriber: user?._id,
+    channel: channelId,
+  });
 
-    if(existingSubscription){
-        message = "unsubscribed successfully"
-    } else{
-       newsubscription =  await subscriptionModel.create({
-            subscriber:user?._id,
-            channel: channelId
-        })
-        message = "subscribed successfully"
-    }
+  let message = "";
+  let newsubscription = null;
 
+  if (existingSubscription) {
+    message = "unsubscribed successfully";
+  } else {
+    newsubscription = await subscriptionModel.create({
+      subscriber: user?._id,
+      channel: channelId,
+    });
+    message = "subscribed successfully";
+  }
 
-    return res
-           .status(200)
-           .json(
-            new apiResponse(
-                200, 
-                newsubscription,
-                message
-            )
-           ) 
-
-})
-
+  return res.status(200).json(new apiResponse(200, newsubscription, message));
+});
 
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
+  const { channelId } = req.params;
 
-    const subscribers = await subscriptionModel.aggregate([
-        {
-            $match: {
-                channel: new mongoose.Types.ObjectId(channelId)
-            }
-        },
-        {
-            $project: {
-                subscriber: 1,
-            }
-        }
-    ])
+  const subscribers = await subscriptionModel.aggregate([
+    {
+      $match: {
+        channel: new mongoose.Types.ObjectId(channelId),
+      },
+    },
+    {
+      $project: {
+        subscriber: 1,
+      },
+    },
+  ]);
 
-    if(subscribers.length === 0){
-        throw new apiError(400, "cant find subscribers")
-    }
+  if (subscribers.length === 0) {
+    throw new apiError(400, "cant find subscribers");
+  }
 
-    return res.status(200).json(new apiResponse(200, subscribers[0], "user subscribers fetched successfully"))
-})
-
+  return res
+    .status(200)
+    .json(
+      new apiResponse(
+        200,
+        subscribers[0],
+        "user subscribers fetched successfully"
+      )
+    );
+});
 
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const { subscriberId } = req.params
-})
+  const { subscriberId } = req.params;
+
+  const channels = await subscriptionModel.aggregate([
+    {
+      $match: {
+        subscriber: new mongoose.Types.ObjectId(req.user._id || subscriberId),
+      },
+    },
+    {
+      $project: {
+        channel: 1,
+      },
+    },
+  ]);
+
+  if (channels.length === 0) {
+    throw new apiError(400, "cant find subscribers");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new apiResponse(200, channels, "user subscribers fetched successfully")
+    );
+});
 
 
-export {
-    toggleSubscription,
-    getSubscribedChannels,
-    getUserChannelSubscribers
+export { 
+    toggleSubscription, 
+    getSubscribedChannels, 
+    getUserChannelSubscribers 
 }
