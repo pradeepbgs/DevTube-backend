05a3e374640a888f30cfb089c2da452e7df856ba
Pradeep Kumar <114051448+Pradeepbgs@users.noreply.github.com>
2024-01-28 14:49:47 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import bodyparser from 'body-parser'
 const app = express()
 
 app.use(cors({
-    origin: 'http://localhost:3000/',
+    origin: 'http://localhost:5173/',
     credentials: true
 }))
 
