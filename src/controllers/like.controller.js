ff918552720424130d2c58bf3a8cdd2d2111669e
pradeep <pradeep>
2024-02-14 16:10:42 +0530

    improved like controller for like and unlike

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -5,96 +5,123 @@ import { apiResponse } from "../utils/apiResponce.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const { videoId } = req.params;
-    if (!videoId || !isValidObjectId(videoId)) {
-        return res.status(400).json(new apiError(400, "Invalid video ID"));
-    }
+  const { videoId } = req.params;
+  if (!videoId || !isValidObjectId(videoId)) {
+    return res.status(400).json(new apiError(400, "Invalid video ID"));
+  }
 
-    const authenticatedId = req.user?._id;
-    const liked = await Like.create({
-        video: videoId,
-        likedBy: authenticatedId
+  const authenticatedId = req.user?._id;
+  const existingLiked = await Like.findOneAndDelete({
+    video: videoId,
+    likedBy: authenticatedId,
+  });
+
+  let message = "";
+  let newLikedVideo = null;
+
+  if (existingLiked) {
+    message = "unliked video";
+  } else {
+    newLikedVideo = await Like.create({
+      video: videoId,
+      likedBy: authenticatedId,
     });
-
-    if (!liked) {
-        return res.status(400).json(new apiError(400, "Unable to like video"));
+    if (!newLikedVideo) {
+      return res.status(400).json(new apiError(400, "Unable to like video"));
     }
+    message = "liked video";
+  }
 
-    return res.status(200).json(new apiResponse(200, "Like toggled"));
+  return res.status(200).json(new apiResponse(200, newLikedVideo, message));
 });
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const { commentId } = req.params;
-    if (!commentId || !isValidObjectId(commentId)) {
-        return res.status(400).json(new apiError(400, "Invalid comment ID"));
-    }
+  const { commentId } = req.params;
+  if (!commentId || !isValidObjectId(commentId)) {
+    return res.status(400).json(new apiError(400, "Invalid comment ID"));
+  }
 
-    const authenticatedId = req.user?._id;
-    if (!authenticatedId) {
-        return res.status(400).json(new apiError(400, "Unable to like comment"));
-    }
+  const authenticatedId = req.user?._id;
+  const existingLiked = await Like.findOneAndDelete({
+    comment: commentId,
+    likedBy: authenticatedId,
+  });
 
-    const likedComment = await Like.create({
-        comment: commentId,
-        likedBy: authenticatedId
+  let message = "";
+  let newLikedComment = null;
+
+  if (existingLiked) {
+    message = "unliked comment";
+  } else {
+    newLikedComment = await Like.create({
+      video: videoId,
+      likedBy: authenticatedId,
     });
-
-    if (!likedComment) {
-        return res.status(400).json(new apiError(400, "Unable to like comment"));
+    if (!newLikedComment) {
+      return res.status(400).json(new apiError(400, "Unable to like video"));
     }
+    message = "liked comment";
+  }
 
-    return res.status(200).json(new apiResponse(200, "Liked comment"));
+  return res.status(200).json(new apiResponse(200, newLikedComment, message));
 });
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const { tweetId } = req.params;
-    if (!tweetId || !isValidObjectId(tweetId)) {
-        return res.status(400).json(new apiError(400, "Invalid tweet ID"));
-    }
+  const { tweetId } = req.params;
+  if (!tweetId || !isValidObjectId(tweetId)) {
+    return res.status(400).json(new apiError(400, "Invalid tweet ID"));
+  }
 
-    const authenticatedId = req.user?._id;
-    if (!authenticatedId) {
-        return res.status(400).json(new apiError(400, "Unable to like tweet"));
-    }
+  const authenticatedId = req.user?._id;
+  const existingLiked = await Like.findOneAndDelete({
+    video: videoId,
+    likedBy: authenticatedId,
+  });
 
-    const likedTweet = await Like.create({
-        tweet: tweetId,
-        likedBy: authenticatedId
+  let message = "";
+  let newLikedTweet = null;
+
+  if (existingLiked) {
+    message = "unliked tweet";
+  } else {
+    newLikedTweet = await Like.create({
+      tweet: tweetId,
+      likedBy: authenticatedId,
     });
-
-    if (!likedTweet) {
-        return res.status(400).json(new apiError(400, "Unable to like tweet"));
+    if (!newLikedTweet) {
+      return res.status(400).json(new apiError(400, "Unable to like video"));
     }
+    message = "liked tweet";
+  }
 
-    return res.status(200).json(new apiResponse(200, "Liked tweet"));
+  return res.status(200).json(new apiResponse(200, newLikedTweet, message));
 });
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-    const authenticatedId = req.user?._id;
-    if (!authenticatedId) {
-        return res.status(400).json(new apiError(400, "Unable to get liked videos"));
-    }
+  const authenticatedId = req.user?._id;
+  if (!authenticatedId) {
+    return res
+      .status(400)
+      .json(new apiError(400, "Unable to get liked videos"));
+  }
 
-    const likedVideos = await Like.aggregate([
-        {
-            $match: { likedBy: new mongoose.Types.ObjectId(authenticatedId) }
-        },
-        {
-            $lookup: {
-                from: "videos",
-                localField: "video",
-                foreignField: "_id",
-                as: "likedVideos"
-            }
-        }
-    ]);
+  const likedVideos = await Like.aggregate([
+    {
+      $match: { likedBy: new mongoose.Types.ObjectId(authenticatedId) },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "video",
+        foreignField: "_id",
+        as: "likedVideos",
+      },
+    },
+  ]);
 
-    return res.status(200).json(new apiResponse(200, likedVideos[0], "Liked videos"));
+  return res
+    .status(200)
+    .json(new apiResponse(200, likedVideos[0], "Liked videos"));
 });
 
-export {
-    toggleCommentLike,
-    toggleTweetLike,
-    toggleVideoLike,
-    getLikedVideos
-};
+export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos };
