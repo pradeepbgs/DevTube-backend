fb9984da317b40cabaa65abb046e756ee2ed6a73
pradeep <kumarpradeepbgs@gmail.com>
2024-02-26 11:26:30 +0530

    chenges in cluster.js

--- src/cluster.js
+++ src/cluster.js
@@ -1,38 +1,39 @@
 import cluster from 'cluster';
 import os from 'os';
 import app from './app.js';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import connectDB from './db/index.js';
 
-dotenv.config()
+dotenv.config();
 
 const cpus = os.cpus().length;
 
 if (cluster.isPrimary) {
   console.log(`Primary ${process.pid} is running`);
 
-  // Fork workers equal to the number of CPUs
   for (let i = 0; i < cpus; i++) {
     cluster.fork();
   }
 
-  // Listen for dying workers and fork a new one
-  cluster.on('exit', (worker, code, signal) => {
+  cluster.on('exit', (worker) => {
     console.log(`Worker ${worker.process.pid} died. Restarting...`);
     cluster.fork();
   });
 } else {
-    connectDB()
-    .then(() =>(
-        app.listen(process.env.PORT || 3000, () =>{
-            console.log(`app is running on http://localhost:${process.env.PORT}`)
-        })
-    ))
-    .catch((err) => (
-        console.log('mongoDB error on index,js::', err)
-    ))
-    app.on("error", (error) =>{
-        console.log("Error: ",error);
-        throw error;
+  connectDB()
+    .then(() => {
+      const port = process.env.PORT || 3000;
+      app.listen(port, () => {
+        console.log(`App is running on http://localhost:${port}`);
+      });
     })
+    .catch((err) => {
+      console.log('MongoDB connection error:', err);
+      process.exit(1); 
+    });
+
+  app.on('error', (error) => {
+    console.log('App error:', error);
+    process.exit(1); 
+  });
 }
