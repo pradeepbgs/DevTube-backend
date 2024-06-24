7a1ccb64bc1077e2b2276d6c776ef51fe265a3a5
pradeep <kumarpradeepbgs@gmail.com>
2024-06-24 12:27:15 +0530

    cors problem

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cookieParser from 'cookie-parser'
 const app = express()
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://fsvideo.vercel.app',
     credentials: true,
 }))
 
