24147d80fbc142798e31324445453c5a3c9eae70
pradeepbgs <kumarpradeepbgs@gmail.com>
2024-07-21 20:37:14 +0530

    buntest

--- src/bun.test.js
+++ src/bun.test.js
@@ -1,7 +1,7 @@
 import { test } from "bun:test";
 
 test("My API handles 60,000 requests", async () => {
-  for (let i = 0; i < 6000; i++) {
-    const response = await fetch("http://localhost:3000/api/endpoint");
+  for (let i = 0; i < 10000; i++) {
+    const response = await fetch("http://localhost:3000");
   }
 });
