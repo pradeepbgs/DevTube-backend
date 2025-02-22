a1c832b59cee4f7da2c738d27b5c71d5b6166da7
pradeepbgs <kumarpradeepbgs@gmail.com>
2025-02-22 19:23:46 +0530

    new updated frontend

--- dist/assets/History-vHs9rbHf.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-g6IK7Mpq.js";const s=()=>r.jsx("div",{children:"History"});export{s as default};
--- dist/assets/Home-exHR37S6.js
+++
@@ -1,1 +0,0 @@
-import{j as s,V as e}from"./index-g6IK7Mpq.js";const r=()=>s.jsx("div",{className:"",children:s.jsx(e,{})});export{r as default};
