9e67027017b69861b095894a6ee26c441c97d0e8
pradeepbgs <kumarpradeepbgs@gmail.com>
2025-02-28 19:50:28 +0530

    improved frontend

--- dist/assets/History-VFp9Mzl0.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-BuqeWj5v.js";const s=()=>r.jsx("div",{children:"History"});export{s as default};
---
+++ dist/assets/History-cXKwr4RN.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-WfECzoqw.js";const s=()=>r.jsx("div",{children:"History"});export{s as default};
